b7bcd0d434731afa470a952214aab5bf94353ec5
Hugo Andrada <hugoandrada77@gmail.com>
2022-09-16 15:53:01 -0300

    alert dialog error message

---
+++ app/src/main/java/com/melvin/ongandroid/utils/AppConstants.kt
@@ -0,0 +1,10 @@
+package com.melvin.ongandroid.utils
+
+object AppConstants {
+
+    // alert dialog message
+    const val SET_TITLE = "Error"
+    const val SET_MESSAGE = "Ha ocurrido un error obteniendo la información"
+    const val POSITIVE_BUTTON = "Reintentar"
+    const val NEGATIVE_BUTTON = "Cancelar"
+}
