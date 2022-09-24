97764a04f83cf0c5165e0be98eb99e0e920e64ba
nelsonjcamacaro <nelsonjcamacaro@gmail.com>
2022-09-24 18:56:10 -0300

    navegation to contact layout created

--- app/src/main/java/com/melvin/ongandroid/view/ContactFragment.kt
+++ app/src/main/java/com/melvin/ongandroid/view/ContactFragment.kt
@@ -6,17 +6,28 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.melvin.ongandroid.R
+import com.melvin.ongandroid.databinding.FragmentActivitiesBinding
+import com.melvin.ongandroid.databinding.FragmentContactBinding
 
 
 class ContactFragment : Fragment() {
+
+    private var _binding: FragmentContactBinding?= null
+    private val binding get() = _binding!!
 
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_contact, container, false)
+        _binding = FragmentContactBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
     }
 
     
