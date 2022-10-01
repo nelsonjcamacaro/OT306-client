8f40d01d293831efd5a26f8bdd8f16c16102767e
santiagoG <santigime972@gmail.com>
2022-10-01 19:57:24 -0300

    Redirigir la app desde el Log In al Sign Up cuando se presione su botón en la vista de inicio de sesión

---
+++ app/src/main/java/com/melvin/ongandroid/view/fragment/SignupFragment.kt
@@ -0,0 +1,26 @@
+package com.melvin.ongandroid.view.fragment
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.melvin.ongandroid.databinding.FragmentSignupBinding
+
+class SignupFragment: Fragment() {
+    private lateinit var binding: FragmentSignupBinding
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentSignupBinding.bind(view)
+        setupLogin()
+    }
+
+    private fun setupLogin(){
+        binding.buttonRegister.setOnClickListener {
+            val action = SignupFragmentDirections.actionSignupFragmentToLoginFragment()
+            findNavController().navigate(action)
+        }
+    }
+
+
+}
