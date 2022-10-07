c22896633040c04402060ebad8d9d75781cc1abe
nelsonjcamacaro <nelsonjcamacaro@gmail.com>
2022-10-07 18:46:31 -0300

    splash screen created

--- app/src/main/java/com/melvin/ongandroid/view/splash/SplashActivity.kt
+++ app/src/main/java/com/melvin/ongandroid/view/splash/SplashActivity.kt
@@ -1,19 +1,23 @@
 package com.melvin.ongandroid.view.splash
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
 import android.widget.Toast
 import androidx.lifecycle.lifecycleScope
 import com.melvin.ongandroid.R
+import com.melvin.ongandroid.view.MainActivity
 import kotlinx.coroutines.launch
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-
+        setTheme(R.style.splashTheme)
         timerSplash()
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
     }
 
     // TICKET OT306-30
