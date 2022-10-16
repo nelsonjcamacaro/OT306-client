ad0ecb461a83119f23fb98d5c2cdd1f75e498084
hugo andrada <hugoandrada77@gmail.com>
2022-10-16 16:07:53 -0300

    new validation

--- app/src/main/java/com/melvin/ongandroid/viewmodel/validation/ValidationViewModel.kt
+++ app/src/main/java/com/melvin/ongandroid/viewmodel/validation/ValidationViewModel.kt
@@ -1,44 +1,49 @@
 package com.melvin.ongandroid.viewmodel.validation
 
-import android.util.Patterns
+import androidx.lifecycle.MediatorLiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.combine
 
 class ValidationViewModel() : ViewModel() {
 
-    private val _name = MutableStateFlow("")
-    private val _email = MutableStateFlow("")
-    private val _password = MutableStateFlow("")
-    private val _repeatPassword = MutableStateFlow("")
+    val nameData = MutableLiveData<String>()
+    val emailData = MutableLiveData<String>()
+    val passwordData = MutableLiveData<String>()
+    val repeatPasswordData = MutableLiveData<String>()
 
-    fun setName(name: String) {
-        _name.value = name
+    val isValidData = MediatorLiveData<Boolean>().apply {
+        this.value = false
+        addSource(nameData) { name->
+            val email = emailData.value
+            val password = passwordData.value
+            val repPassword = repeatPasswordData.value
+            this.value = validateForm(name, email, password, repPassword)
+        }
+        addSource(emailData) { email->
+            val name = nameData.value
+            val password = passwordData.value
+            val repPassword = repeatPasswordData.value
+            this.value = validateForm(name, email, password, repPassword)
+        }
+        addSource(passwordData) { password->
+            val email = emailData.value
+            val name = nameData.value
+            val repPassword = repeatPasswordData.value
+            this.value = validateForm(name, email, password, repPassword)
+        }
+        addSource(repeatPasswordData) { repeatPassw->
+            val password = passwordData.value
+            val email = emailData.value
+            val name = nameData.value
+            this.value = validateForm(name, email, password, repeatPassw)
+        }
     }
 
-    fun setEmail(email: String) {
-        _email.value = email
-    }
-
-    fun setPassword(password: String) {
-        _password.value = password
-    }
-
-    fun setRepeatPassword(repeatPassword: String) {
-        _repeatPassword.value = repeatPassword
-    }
-
-    val isSubmitButtonRegister: Flow<Boolean> = combine(
-        _name,
-        _email,
-        _password,
-        _repeatPassword
-    ) { name, email, password, repeatPassword ->
-        val isNameCorrect = name.length > 3
-        val isEmailCorrect = email.matches(Patterns.EMAIL_ADDRESS.toRegex())
-        val isPasswordCorrect = password.length > 8
-        val isPasswordMatch = repeatPassword == password
-        return@combine isNameCorrect && isEmailCorrect && isPasswordCorrect && isPasswordMatch
+    private fun validateForm(name: String?, email: String?, password: String?, repPassword: String?): Boolean {
+        val isValidName = name != null && name.isNotBlank() && name.length > 3
+        val isValidEmail = email != null && email.isNotBlank() && email.contains("@")
+        val passwordIsValid = password != null && password.isNotBlank() && password.length > 8
+        val isValidRepeatPassword = repPassword == password
+        return isValidName && isValidEmail && passwordIsValid && isValidRepeatPassword
     }
 }
